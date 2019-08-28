43871aa8e62aa8fec70a1ab1ab52c666801ab779
Maria Pelaez <maria.shecodes@gmail.com>
2019-08-28 13:04:17 +0200

    deploy02

--- myprojectserver/app.js
+++ myprojectserver/app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
-const favicon      = require('serve-favicon');
 const logger       = require('morgan');
 const path         = require('path');
 const cors         = require('cors')
@@ -61,9 +60,7 @@ app.use(passport.session())
 
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
